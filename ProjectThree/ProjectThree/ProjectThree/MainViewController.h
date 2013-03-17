c38ec47905204d2b0d3bf7f9c0c89b9a94734395
Dale Tupling <tupling32@hotmail.com>
2013-03-17 22:57:03 +0430

    Project Three
    Project three files uploaded. Started layout of application.

---
+++ ProjectThree/ProjectThree/ProjectThree/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  ProjectThree
+//
+//  Created by Dale Tupling on 3/17/13.
+//  Copyright (c) 2013 Dale Tupling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ ProjectThree/ProjectThree/ProjectThree/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ProjectThree
+//
+//  Created by Dale Tupling on 3/17/13.
+//  Copyright (c) 2013 Dale Tupling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MainAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MainAppDelegate class]));
+    }
+}
